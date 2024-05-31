471b8ed1a962dd122303e5d2a52f68613e40038c
Thomas <34007164+kai7win@users.noreply.github.com>
2024-05-31 11:47:40 +0800

    Initial Commit

---
+++ PDFKitImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDFKitImage
+//
+//  Created by Thomas on 2024/5/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFKitImage/PDFKitImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitImageApp.swift
+//  PDFKitImage
+//
+//  Created by Thomas on 2024/5/31.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
